a74c1ba6e2936f597e432c4e1521e3430c156fd8
Wyatt <parsonswy@gmail.com>
2023-10-15 01:46:13 -0400

    make bot better

--- src/listeners/message/Nums.js
+++ src/listeners/message/Nums.js
@@ -1,23 +1,53 @@
+const msPerHour = 3600000;
+function getCurrentUTCEpoch() {
+	const now = new Date();
+  const utcDate = new Date(Date.UTC(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds()
+  ));
+  return utcDate;
+}
+
+function ceilToNearestHour(timestamp) {
+  return Math.ceil(timestamp / msPerHour) * msPerHour;
+}
+
 const Nums = {
 
-	if: (event) => event.content.trim() === '.nums',
+	if: (event) => event.content.trim().startsWith('.nums'),
 
 	on: async (event) => {
-		const now = new Date();
-		const start = now.getHours() + 1;
-		let rounds = 6;
-		if (now.getHours() < 4) {
-			rounds = 4 - now.getHours();
+		let [,intervals = 6, offset=0] = event.content.trim().split(' ');
+		const now = getCurrentUTCEpoch();
+
+		if (typeof intervals === 'string' && intervals.trim() == 'help') {
+			await event.channel.send('`.nums [intervals: how many timeslots you want; IE 4 times] [offset: number of hours from now to start the times; IE 2 hours from now]`');
+			return;
 		}
-		const nextHourToday = new Date(`${now.getFullYear()} ${now.getMonth()+1} ${now.getDate()} ${start}:00:00`);
-		
+
+		intervals = parseInt(intervals, 10);
+		offset = parseInt(offset, 10);
+
+		if (intervals > 10 || offset > 12 || isNaN(intervals) || isNaN(offset)) {
+			await event.channel.send('Nice try, <:brogrammer:983524835768557598>.');
+			return;
+		} else if (intervals < 1 || offset < 0) {
+			await event.channel.send('Nice try, <:brogrammer:983524835768557598>.');
+			return;
+		}
+
+		const hourOffset = offset  * msPerHour;
 		await event.channel.send(`<@${global.l.getNumsPingGroup()}>`);
-		for (let i=0; i<rounds; i++) {
-			const epochSeconds = (nextHourToday.getTime() / 1000) + i * 3600;
-			const message = await event.channel.send(`<t:${epochSeconds}>`);
+		for (let i=0; i<intervals; i++) {
+			const epochSeconds = now.getTime() + (i * msPerHour + hourOffset);
+			const epochSecondsToTheNextHour = ceilToNearestHour(epochSeconds, 7) / 1000;
+			const message = await event.channel.send(`<t:${epochSecondsToTheNextHour}>`);
 			await message.react(global.l.getConfig().getNumsReactionEmote())
 		}
- 
 	}
 }
 
